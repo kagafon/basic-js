eaa8dbea4f86808a0dee7f82007f01db52cb4ab3
kagafon <k.agafonov@ase-ec.ru>
2020-02-17 01:23:34 +0300

    feat: "Depth calculator" feature implementation

--- src/recursive-depth.js
+++ src/recursive-depth.js
@@ -1,6 +1,21 @@
 module.exports = class DepthCalculator {
-    calculateDepth(/* arr */) {
-        throw 'Not implemented';
-        // remove line with error and write your code here
+    maxDepth = 0;
+    currentLevel = 0;
+    calculateDepth(arr, isInternalRun) {
+        if (!isInternalRun){
+            this.maxDepth = 0;
+            this.currentLevel = 0;
+        } 
+        if (arr instanceof Array){
+            this.currentLevel += 1;
+            arr.forEach(x => {
+                if (x instanceof Array){
+                     this.calculateDepth(x, true);
+                }
+            });
+            if (this.currentLevel > this.maxDepth) this.maxDepth = this.currentLevel;
+            this.currentLevel -= 1;
+        }
+        return this.maxDepth;
     }
 };
